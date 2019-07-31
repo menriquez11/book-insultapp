54318971b5b712abb156b8d3995fc1b54c9d708b
sh4dow1115 <mario.enriquez9411@gmail.com>
2019-07-30 23:13:44 -0500

    fIX InsultGenerator.java DB connection

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -6,28 +6,27 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class InsultGenerator {
+
 	public String generateInsult() {
 		String vowels = "AEIOU";
 		String article = "an";
 		String theInsult = "";
+
 		try {
 			String databaseURL = "jdbc:postgresql://";
-			databaseURL = databaseURL + System.getenv("POSTGRESQL_SERVICE_HOST");
-			databaseURL = databaseURL+ "/" + System.getenv("POSTGRESQL_DATABASE");
-			
+			databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
+			databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
+
 			String username = System.getenv("POSTGRESQL_USER");
 			String password = System.getenv("PGPASSWORD");
-			
 			Connection connection = DriverManager.getConnection(databaseURL, username, password);
-			
-			if(connection != null) {
-				String query = "select a.string AS first, b.string AS second, c.string AS noun from"
-						+ "short_adjective a, long_adjective b, noun c ORDER BY random() limit 1";
-				
+
+			if (connection != null) {
+				String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
 				Statement stmt = connection.createStatement();
-				ResultSet rs = stmt.executeQuery(query);
-				while(rs.next()) {
-					if(vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
+				ResultSet rs = stmt.executeQuery(SQL);
+				while (rs.next()) {
+					if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
 						article = "a";
 					}
 					theInsult = String.format("Thou art %s %s %s %s!", article, rs.getString("first"),
@@ -37,7 +36,7 @@ public class InsultGenerator {
 				connection.close();
 			}
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 			return "Database connection problem!";
 		}
 		return theInsult;
